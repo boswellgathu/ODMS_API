a507cc91fe3ca88e1f433c090225bd3ab943fcfb
GathuBoswell <gathudev@gmail.com>
2017-03-21 22:37:35 +0300

    chore[fix-tests-coverage]: Remove unnecessary code

--- server/controllers/AuthHandler.js
+++ server/controllers/AuthHandler.js
@@ -6,18 +6,6 @@ const User = db.User;
 const secret = config.secret;
 
 class AuthHandler {
-  static UserInfo(user) {
-    return {
-      role: user.roleId,
-      email: user.email,
-      userId: user.id
-    }
-  }
-  static GenerateToken(user) {
-    return jwt.sign(UserInfo(user), secret, {
-      expiresIn: 60 * 60 * 24 // 24 Hours
-    });
-  }
   static VerifyToken(req, res, next) {
     const token = req.body.token || req.query.token || req.headers['x-access-token'];
     if (token) {
